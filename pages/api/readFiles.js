a325c60ca597e63c2a97b845e302da2f01bbb30c
Joe Hingston <joe.hingston@worktribe.com>
2022-03-02 22:00:30 +0000

    fix

--- pages/api/readFiles.js
+++ pages/api/readFiles.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fs from 'fs'
-import path from 'path'
+var fs = require('fs');
+var path = require('path');
 
 export default function handler(req, res) {
   const dirRelativeToPublicFolder = 'photos'
